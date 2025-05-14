552369518b41c004eeb5f1cff65301abe4a18ddc
Rajiv Kumar <kumarrajiv0920@gmail.com>
2025-05-14 19:52:22 +0530

    NoteNest App

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,21 +1,10 @@
 import admin from "firebase-admin";
-import dotenv from "dotenv";
 
-dotenv.config();
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    type: "service_account",
-    private_key_id: "78149d053b5d1b3ce72f4c028a053792b8b519bb",
-    client_id: "103851249611148573135",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40dotted-tube-459612-h7.iam.gserviceaccount.com",
-    universe_domain: "googleapis.com",
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
   }),
 });
 
