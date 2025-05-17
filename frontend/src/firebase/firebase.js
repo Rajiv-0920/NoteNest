56ec27b53a3b2571407ad3632c230c664b146d84
Rajiv Kumar <kumarrajiv0920@gmail.com>
2025-05-17 19:49:54 +0530

    security update

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ1NOq4stLZ9_EmfZrb-tWRHeYXwYxHGQ",
-  authDomain: "dotted-tube-459612-h7.firebaseapp.com",
-  projectId: "dotted-tube-459612-h7",
-  storageBucket: "dotted-tube-459612-h7.firebasestorage.app",
-  messagingSenderId: "459542281976",
-  appId: "1:459542281976:web:271a646e01750b3ce9450d",
-  measurementId: "G-9HV5SHS02G",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
