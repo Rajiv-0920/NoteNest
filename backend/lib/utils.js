30b06fbca33ac6f95584bf12e9de1f1f3d47a61b
Rajiv <kumarrajiv0920@gmail.com>
2026-04-02 23:07:54 +0530

    fix: fix cookie settings for cross-domain production

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: 'none',
-    secure: process.env.NODE_ENV === 'development',
+    secure: true,
   });
 
   return token;
